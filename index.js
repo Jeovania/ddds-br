963fdf0c7bd6f2390181c7436b9e448c131dcc68
Pedro Alves <pedromarcelodesaalves@gmail.com>
2019-02-15 00:02:03 -0300

    Listando cidades que aceitam um determinado DDD

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import ddds from './DDDs.json';
+import cidades from './cidades.json';
 
 function localPorDDD(ddd='') {
     const res = ddds.find(el => (el['ddd'] === ddd.toString()));
     return (res !== undefined) ? res.local : '';
 }
 
-export { ddds, localPorDDD };
+function listaDeCidades(ddd) {
+    return cidades[ddd] ? cidades[ddd] : [];
+}
+
+export { ddds, localPorDDD, listaDeCidades };
